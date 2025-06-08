f8aa981a12e7f27995b34aeee3499f431e160de6
Sangeetha074 <Sangeethamuthukrishnan06@gmail.com>
2025-06-08 14:13:51 +0530

    update

---
+++ home.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Loginpage from './loginpage'
+
+export const Home = () => {
+  return (
+    <Loginpage></Loginpage>
+  )
+}
--- router.jsx
+++ router.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import {Routes,Route} from'react-router-dom'
-import Loginpage from './loginpage'
 import Signup from './signup'
 import Register from './register'
 import Page from './page'
 import Password from './password'
 import Form from './form'
+import { Home } from './home'
+import Loginpage from './loginpage'
+
 const Router = () => {
     return (
       <div>
         <Routes>
           <Route>
+            <Route path="/" element={<Home/>}></Route>
             <Route path="/" element={<Loginpage/>}></Route>
             <Route path="/signup" element={<Signup/>}></Route>
             <Route path="/register" element={<Register/>}></Route>
